dafa362ca0c35be1230f3d4b73e5381d87963c94
Njamen <njamen.n3@gmail.com>
2022-12-13 14:45:28 -0800

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_to_98 - check is a randomly given number is positive or negative
+ * @n: d
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 98)
+	{
+		for (i = n ; i >= 97 ; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("98\n");
+	} else
+	{
+		for (i = n ; i <= 97 ; i++)
+		{
+			printf("%d, ", i);
+		};
+		printf("98\n");
+	}
+}
