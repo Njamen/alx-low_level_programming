49efc4e28108ddf94988940757bfe7b1d288c720
Njamen <njamen.n3@gmail.com>
2022-12-13 11:21:20 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_sign - check is a randomly given number is positive or negativ
+ * @c: given char
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	};
+}
