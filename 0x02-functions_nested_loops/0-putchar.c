b46130b8b21c083ee885c10aa77ef02a650982e0
Njamen <njamen.n3@gmail.com>
2022-12-13 02:36:59 -0800

    c1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - check is a randomly given number is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
