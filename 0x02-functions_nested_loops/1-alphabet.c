1bdd2f040bf8148d14e899f4cb0474a490e66bd3
Njamen <njamen.n3@gmail.com>
2022-12-13 03:36:38 -0800

    kk

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_alphabet - check is a randomly given number is positive or negative
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	};
+	_putchar('\n');
+
+	return ;
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - check is a randomly given number is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
