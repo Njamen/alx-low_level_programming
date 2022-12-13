2a0d117f104b153270f149ab6dce3327732a09eb
Njamen <njamen.n3@gmail.com>
2022-12-13 14:04:57 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * add - check is a randomly given number is positive or negative
+ * @a: a
+ * @b: b
+ *
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
