0a23636104b9fc70e029662a345e90e58c5533fc
Njamen <njamen.n3@gmail.com>
2022-12-13 12:08:22 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_last_digit - check is a randomly given number is positive or negativ
+ * @n: given char
+ *
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
