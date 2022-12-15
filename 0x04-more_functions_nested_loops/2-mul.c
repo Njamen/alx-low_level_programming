c08239c4181a9e72c65611a24d923395740efc53
Njamen <njamen.n3@gmail.com>
2022-12-15 03:38:26 -0800

    addddd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * mul - check is a randomly given number is positive or negativ
+ * @a: given char
+ * @b: ddd
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
