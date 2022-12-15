f3625f8f354e538de74f764f64ccf89cbf4175ec
Njamen <njamen.n3@gmail.com>
2022-12-15 04:01:06 -0800

    oo

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_numbers - check is a randomly given number is positive or negativ
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		_putchar(i);
+	};
+	_putchar('\n');
+
+}
