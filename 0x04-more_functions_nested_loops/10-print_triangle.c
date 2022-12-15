bdbcefb2179e8e75858821ce1275fc3a464da434
Njamen <njamen.n3@gmail.com>
2022-12-15 05:38:46 -0800

    jjhh

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_triangle - check is a randomly given number is positive or negativ
+ * @size: - ee
+ *
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (j = 1 ; j <= size ; j++)
+	{
+		for (i = 1 ; i <= (size - j) ; i++)
+		{
+			_putchar(' ');
+		};
+		for (i = 1 ; i <= j ; i++)
+		{
+			_putchar('#');
+		};
+		_putchar('\n');
+	};
+}
