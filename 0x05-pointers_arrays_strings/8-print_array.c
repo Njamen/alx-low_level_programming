5ca9b8d0a7c8f45f1845195eb89e314d4ada822e
Njamen <njamen.n3@gmail.com>
2022-12-19 04:52:29 -0800

    sadasd

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * print_array - check is a randomly given number is positive or negativ
+ * @a: given char
+ * @n: ddd
+ *
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		return;
+	}
+	while (i < (n - 1))
+	{
+		printf("%d, ", *(a + i));
+		i = i + 1;
+	}
+	printf("%d\n", *(a + (n - 1)));
+}
