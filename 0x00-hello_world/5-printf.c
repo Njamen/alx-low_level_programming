11c7872fbac2859c4015d2161a460e0a2bf7f82a
Njamen <njamen.n3@gmail.com>
2022-12-19 07:22:28 -0800

    cmnjhn

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - check is a randomly given number is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
