80a2d67350615c8fba2df6373accd0b86008235d
Njamen <njamen.n3@gmail.com>
2022-12-19 04:22:25 -0800

    shjdye

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * puts_half - check is a randomly given number is positive or negativ
+ * @str: given char
+ *
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int n = 0;
+
+	if (*str == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	while (*(str + n) != '\0')
+	{
+		n = n + 1;
+	}
+	n = n / 2;
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+		n = n + 1;
+	}
+	_putchar('\n');
+}
