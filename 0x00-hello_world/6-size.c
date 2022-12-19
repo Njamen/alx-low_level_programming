34e8935e2293ea536db2caa3c0b4994a7c4f2577
Njamen <njamen.n3@gmail.com>
2022-12-19 07:33:17 -0800

    ddeee

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - check is a randomly given number is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char);			
+	printf("Size of a int: %d byte(s)\n", sizeof(int);
+	printf("Size of a long int: %d byte(s)\n", sizeof(int);
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long);
+	printf("Size of a float: %d byte(s)\n", sizeof(float);
+	return (0);
+}
