c388b7910baaa601bbb5f101f56149163898fc3a
Njamen <njamen.n3@gmail.com>
2022-12-19 00:28:59 -0800

    www

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * reset_to_98 - check is a randomly given number is positive or negativ
+ * @n: given char
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
