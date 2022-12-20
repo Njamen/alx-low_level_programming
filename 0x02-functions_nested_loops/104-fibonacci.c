b70198b68d7e9c58e67e4b512b374656a3969e26
Njamen <njamen.n3@gmail.com>
2022-12-20 02:48:36 -0800

    sss

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - check is a randomly given number is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	float n_0 = 1;
+	float n_1 = 2;
+	int i = 3;
+	float n;
+
+	printf("%.0f, ", n_0);
+	printf("%.0f", n_1);
+	while (i <= 98)
+	{
+		n = n_0 + n_1;
+		printf(", %.0f", n);
+		n_0 = n_1;
+		n_1 = n;
+		i = i + 1;
+	}
+	printf("\n");
+	return (0);
+}
