5011e6d791d821b9a809d3be0529c2ef9797817b
Njamen <njamen.n3@gmail.com>
2023-01-04 16:26:42 -0800

    adasas

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * factoriel   - eheck is a randov
+ * @n: given char
+ *
+ * Return: 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
