45f6835f6d03f5369872b0432d51342303e79e3f
Njamen <njamen.n3@gmail.com>
2023-01-13 04:04:24 -0800

    adasas

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * malloc_checked- check is a randomly given number is positive or negative
+ * @b: ...
+ *
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *space;
+
+	space = (char*)malloc(b);
+	return (space);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,12 @@
+0x0C. C - More malloc, free
+
+## Introduction
+This repository contains program that illustrate how to use malloc, free.
+
+## List of function and thier description
+
+	* 0-malloc_checked.c : contains a function that allocates memory using malloc.
+
+
+
+
