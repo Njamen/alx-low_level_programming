3dd252e8c0b8a934d18eb686f7a8f72b83a0cef8
Njamen <njamen.n3@gmail.com>
2023-01-13 05:58:15 -0800

    adasas

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,6 +21,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	n1 = strlen(s1);
 	n2 = strlen(s2);
+	if (n <= n2)
+		n2 = n;
 	rs = (char *)malloc(sizeof(char) * (n1 + n2 + 1));
 	if (rs == NULL)
 		return (NULL);
