edfd320b0aca17db8085ec26113ef24cea22728f
Njamen <njamen.n3@gmail.com>
2023-01-17 07:42:47 -0800

    adasas

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ *  struct dog - personal animal
+ *  @name: -
+ *  @age: -
+ *  @owner: -
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
