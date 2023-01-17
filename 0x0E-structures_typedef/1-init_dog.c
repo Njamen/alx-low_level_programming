c35a25109e7e43d5b83fdf86ec16fc860e480731
Njamen <njamen.n3@gmail.com>
2023-01-17 08:02:34 -0800

    adasas

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /* more headers goes there */
 
 /**
@@ -12,6 +13,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
