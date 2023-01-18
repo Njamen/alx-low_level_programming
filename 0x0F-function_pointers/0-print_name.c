8b0f67890713fd03cdad6f30523e1a2b2d5f5332
Njamen <njamen.n3@gmail.com>
2023-01-18 06:05:08 -0800

    adasas

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * print_name -  ...
+ * @name: ...
+ * @f: .
+ *
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
