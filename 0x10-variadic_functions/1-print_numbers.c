7d5b6ca6a2bc9b48b4464e13177495006cfe4946
Njamen <njamen.n3@gmail.com>
2023-01-22 17:40:02 -0800

    adasas

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ptr, n);
 	for (i = 0 ; i < n - 1 ; i++)
 	{
-		printf("%d%s ", va_arg(ptr, int), separator);
+		printf("%d%s", va_arg(ptr, int), separator);
 	}
 	printf("%d\n", va_arg(ptr, int));
 	va_end(ptr);
