b083cac8c1b083c63f31ae8a7ee3326266f6fd18
Njamen <njamen.n3@gmail.com>
2023-01-24 03:26:41 -0800

    adasas

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -20,28 +20,28 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
-			b = 0;
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(args, int));
-					break;
-				case 's':
-					printf("%s", va_arg(args, char *));
-					break;
-				case 'i':
-					printf("%d", va_arg(args, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(args, double));
-					break;
-				default:
-					b = 1;
-					break;
-			}
-			if (format[i + 1] != '\0' && b != 1)
-				printf(", ");
-			i++;
+		b = 0;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 's':
+				printf("%s", va_arg(args, char *));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			default:
+				b = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && b != 1)
+			printf(", ");
+		i++;
 	}
 	va_end(args);
 	printf("\n");
