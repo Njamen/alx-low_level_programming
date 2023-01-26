32d103024f0074e7653f658fc781bc026dc6b630
Njamen <njamen.n3@gmail.com>
2023-01-26 06:15:03 -0800

    adasas

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * free_list - function that returns
+ * the number of elements in a linked list_t list.
+ * @head: ...
+ *
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	if (head->next != NULL)
+		free_list(head->next);
+	free(head);
+}
