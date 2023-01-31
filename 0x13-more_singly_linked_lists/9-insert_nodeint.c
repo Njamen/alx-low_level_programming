08c58174212f8a3f7213cb3f68beaaa640ebe494
Njamen <njamen.n3@gmail.com>
2023-01-31 16:13:33 +0100

    adasas

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 	if (*head == NULL && idx != 0)
 		return (NULL);
