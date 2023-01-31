6e48cd7b6c1d9122636e8943b8653b3a7c34375d
Njamen <njamen.n3@gmail.com>
2023-01-31 02:28:13 +0100

    adasas

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * free_listint2 - function that returns
+ * the number of elements in a linked list_t list.
+ * @head: ...
+ *
+ * Return: 0
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+	if ((*head)->next != NULL)
+		free_listint2(&((*head)->next));
+	free(*head);
+}
