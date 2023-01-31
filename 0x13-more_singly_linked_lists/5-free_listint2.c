48506fe68bb04bbcb59eeb0434954c2692e93ab6
Njamen <njamen.n3@gmail.com>
2023-01-31 03:21:58 +0100

    adasas

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,9 +12,10 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return;
 	if ((*head)->next != NULL)
 		free_listint2(&((*head)->next));
 	free(*head);
+	head = NULL;
 }
