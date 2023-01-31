ec10f6ceb05995fc11afd70f4aea7938848314d4
Njamen <njamen.n3@gmail.com>
2023-01-31 16:49:04 +0100

    adasas

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head == NULL)
 		return (-1);
 	if (*head == NULL && index == 0)
-		return (1);
+		return (-1);
 	if (*head == NULL && index != 0)
 		return (-1);
 	if (index == 0)
