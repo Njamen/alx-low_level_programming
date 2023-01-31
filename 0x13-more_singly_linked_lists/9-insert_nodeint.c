63f5fbdd51a26d93a7a6bc7af6ad27e0dbcde1e7
Njamen <njamen.n3@gmail.com>
2023-01-31 16:11:37 +0100

    adasas

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
+	if (*head == NULL && idx != 0)
+		return (NULL);
 	if (idx == 0)
 	{
 		new = malloc(sizeof(listint_t));
