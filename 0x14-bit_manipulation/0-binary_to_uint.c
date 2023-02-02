bdacc1d26ff864f82a2b6d93fb5e39de0bbf4944
Njamen <njamen.n3@gmail.com>
2023-02-02 16:09:48 +0100

    adasas

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * binary_to_uint - check is a randomly given number is positive or negativ
+ * @b: given char
+ *
+ * Return: 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j, n;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] - '0' > 1)
+			return (0);
+		i = i + 1;
+	}
+	i = i - 1;
+	j = 0;
+	n = 0;
+	while (i >= 0)
+	{
+		n = n + ((int)(b[i] - '0') * (1 << j));
+		j = j + 1;
+		i = i - 1;
+	}
+	return (n);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+#endif
