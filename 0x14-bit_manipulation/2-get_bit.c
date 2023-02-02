1b99f08bbb6f2e6acb1bcd31d2305cc7ce667322
Njamen <njamen.n3@gmail.com>
2023-02-02 20:27:44 +0100

    adasas

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * get_bit - check is a randomly given number is positive or negativ
+ * @n: given char
+ * @index: ddd
+ *
+ * Return: 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long kms = 1;
+	unsigned int i = 0;
+
+
+	if (n == 0)
+	{
+		return (0);
+	}
+/*	while (kms > 0)
+	{
+		if ((kms & n) != 0)
+			break;
+		kms = kms >> 1;
+	} */
+	while (i != index)
+	{
+		if (kms >= n)
+			return (-1);
+		kms = kms << 1;
+		i = i + 1;
+	}
+	if ((kms & n) == 0)
+	{
+		return (0);
+	}
+	else
+		return (1);
+}
