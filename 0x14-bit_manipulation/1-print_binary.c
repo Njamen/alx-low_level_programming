b1af5566111f7e575771d49be5d1fea52a3a08b2
Njamen <njamen.n3@gmail.com>
2023-02-02 16:59:18 +0100

    adasas

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_binary - check is a randomly given number is positive or negativ
+ * @n: given char
+ *
+ * Return: 0
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int kms = 32768;
+	int tm = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (kms > 0)
+	{
+		if ((kms & n) == 0)
+			if (tm != 0)
+				_putchar('0');
+			else
+			{
+			}
+		else
+		{
+			_putchar('1');
+			tm = 1;
+		}
+		kms = kms >> 1;
+	}
+}
