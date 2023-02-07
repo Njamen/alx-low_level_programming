da2bf2321c03104f79c0ce9ece566903ed73ba43
Njamen <njamen.n3@gmail.com>
2023-02-07 08:38:24 +0100

    adasas

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		r = write(textfile, text_content, sizeof(text_content));
+		r = write(textfile, text_content, sizeof(text_content) - 2);
 		if (r != sizeof(text_content))
 			return (-1);
 	}
