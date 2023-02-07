5b59210e99da3a53208d64bd5b23fca2f6415140
Njamen <njamen.n3@gmail.com>
2023-02-07 08:28:58 +0100

    adasas

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,9 +15,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	textfile = open(filename, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (textfile < 0)
-		return (-1);
+	textfile = open(filename, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if (textfile == -1)
+	{
+		textfile = open(filename, O_WRONLY | O_TRUNC);
+		if (textfile < 0)
+			return (-1);
+	}
 	if (text_content != NULL)
 	{
 		r = write(textfile, text_content, sizeof(text_content));
