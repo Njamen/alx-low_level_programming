f8b0e4ec42019e51dfaba53ce76497d12d51f58f
Njamen <njamen.n3@gmail.com>
2023-02-07 08:00:57 +0100

    adasas

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * create_file - check is a randomly given number is positive or negativ
+ * @filename: e
+ * @text_content: d
+ *
+ * Return: 0
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int textfile;
+	ssize_t r;
+
+	if (filename == NULL)
+		return (-1);
+	textfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content != NULL)
+	{
+		r = write(textfile, text_content, sizeof(text_content));
+		if (r != sizeof(text_content))
+			return (-1);
+	}
+	close(textfile);
+	return (1);
+}
+
