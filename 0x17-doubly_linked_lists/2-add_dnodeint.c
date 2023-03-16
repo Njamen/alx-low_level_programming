f499df6e7f845c05c584e92fb74d7d88facb5ea5
Njamen <njamen.n3@gmail.com>
2023-03-16 09:55:03 +0100

    commit saved

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* more headers goes there */
+
+/**
+ * add_dnodeint - Prints all the elements of a list_t list.
+ * @head: ...
+ * @n: ...
+ *
+ * Return: 0
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new != NULL)
+	{
+		new->n = n;
+		new->next = *head;
+		new->prev = NULL;
+		*head = new;
+		return (*head);
+	}
+	else
+		return (*head);
+}
