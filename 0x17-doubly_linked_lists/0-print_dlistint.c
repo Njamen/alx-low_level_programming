0dda537b9b73897589d2dcfaef729a91e2206187
Njamen <njamen.n3@gmail.com>
2023-03-16 09:14:08 +0100

    commit saved

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/* more headers goes there */
+
+/**
+ * print_dlistint - Prints all the elements of a list_t list.
+ * @h: ...
+ *
+ * Return: 0
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	else
+		printf("%d\n", h->n);
+	if (h->next != NULL)
+		return (1 + print_dlistint(h->next));
+	else
+		return (1);
+}
